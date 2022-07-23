f3b1a434c33da88c065cb6b3a905732be8e9bff9
omarvegaabreu <omarvega@hotmail.com>
2022-07-22 20:34:35 -0400

    added, navbar, routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import HomeRoute from "./routes/home/Home.Route.Component";
+import NavBarComponent from "./routes/navbar/Navigation.Component";
 import { Routes, Route } from "react-router-dom";
+
+// import { Home } from "@mui/icons-material";
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomeRoute />}></Route>
+      {/* parent component */}
+      <Route path="/" element={<NavBarComponent />}>
+        <Route index element={<HomeRoute />} />
+        <Route path="/shop" element={<h1>shop</h1>} />
+        {/* parent component end */}
+      </Route>
     </Routes>
   );
 };
