ca357242662e945397648202514b227ef7d9a876
omarvegaabreu <omarvega@hotmail.com>
2022-07-22 23:03:18 -0400

    added, navigation, firebase config

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import HomeRoute from "./routes/home/Home.Route.Component";
 import NavBarComponent from "./routes/navbar/Navigation.Component";
 import { Routes, Route } from "react-router-dom";
+import SingIn from "./routes/login/SingIn.Component";
 
 // import { Home } from "@mui/icons-material";
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Route path="/" element={<NavBarComponent />}>
         <Route index element={<HomeRoute />} />
         <Route path="/shop" element={<h1>shop</h1>} />
+        <Route path="/sign-in" element={<SingIn />} />
         {/* parent component end */}
       </Route>
     </Routes>
---
+++ src/routes/login/SingIn.Component.jsx
@@ -0,0 +1,5 @@
+const SingIn = () => {
+  return <h1>SingIn</h1>;
+};
+
+export default SingIn;
