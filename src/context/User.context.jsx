5122c7cae78b43d1890077cac07ecd76f1a29a83
omarvegaabreu <omarvega@hotmail.com>
2022-09-02 11:37:09 -0400

    added store, migrating to redux

--- src/context/User.context.jsx
+++ src/context/User.context.jsx
@@ -10,25 +10,6 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
-export const USER_ACTION_TYPES = {
-  SET_CURRENT_USER: "SET_CURRENT_USER",
-};
-
-const INITIAL_STATE = {
-  currentUser: null,
-};
-
-const userReducer = (state, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
-    case USER_ACTION_TYPES.SET_CURRENT_USER:
-      return { ...state, currentUser: payload };
-    default:
-      throw new Error(`Unhandled type ${type} in userReducer`);
-  }
-};
-
 export const UserProvider = ({ children }) => {
   const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
 
--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 import "./index.css";
 import App from "./App";
-import { UserProvider } from "./context/User.context";
 import { CategoriesProvider } from "./context/Categories.Context";
 import { CartProvider } from "./context/Cart.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
+    <Provider store={store}>
+      <BrowserRouter>
         <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
         </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
